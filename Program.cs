79f82d1212370e3045cbfbb4c8ac70b9eb2c00cd
sebastianDeLeon <99497363+sebastianDeLeon@users.noreply.github.com>
2022-02-25 09:29:54 -0500

    Amigos

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace wepsysPrueba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cual es su nombre");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("Cuantos nombres ingresara");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("------------------------------------------");
+
+            string[] nombres = new string[n];
+            string[] amigos = new string[n];
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Ingrese nombre "+i);
+                nombres[i] = Console.ReadLine(); 
+
+            }
+            Console.WriteLine("------------------------------------------");
+            for(int i = 0; i < n; i++)
+            {
+                if (nombre.Length == nombres[i].Length)
+                {
+                    amigos[i] = nombres[i];
+                }
+            }
+
+            Console.WriteLine("Sus amigos son: ");
+            for (int i = 0; i < n; i++)
+            {
+                System.Console.WriteLine(amigos[i]);
+            }
+            Console.ReadKey();
+           
+        }
+    }
+}
